e618b73864a57dba4d0076ae0bb58b43784419d1
ikicapitan <ikicapitan141@gmail.com>
2019-08-09 20:54:31 -0300

    9-08

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -8,12 +8,13 @@ var puede_disparar = true
 func _ready():
 	pass # Replace with function body.
 
-func disparar():
+func disparar(var d):
 	if(puede_disparar):
 		var newm = get_tree().get_nodes_in_group("main")[0].romualdo.instance()
 		get_tree().get_nodes_in_group("nivel")[0].add_child(newm)
 		newm.global_position =  $spawn.global_position
-		newm.get_node("Chest").apply_impulse(newm.get_node("Chest").position, Vector2(cos(rotation) * fuerza_disparo,sin(rotation) * fuerza_disparo))
+		var fuerza_actual = d * fuerza_disparo / 1000
+		newm.get_node("Chest").apply_impulse(newm.get_node("Chest").position, Vector2(cos(rotation) * fuerza_actual,sin(rotation) * fuerza_actual))
 		puede_disparar = false
 		yield(get_tree().create_timer(1.0),"timeout")
 		puede_disparar = true
--- Scenes/romualdo.gd
+++ Scenes/romualdo.gd
@@ -3,3 +3,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	
